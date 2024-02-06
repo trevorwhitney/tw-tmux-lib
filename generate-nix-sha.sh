3431a9e40f6532a9d510c91fbec3f0065a5925af
Trevor Whitney <trevorjwhitney@gmail.com>
2024-02-06 14:43:43 -0700

    fix generate sha script

--- generate-nix-sha.sh
+++ generate-nix-sha.sh
@@ -4,10 +4,5 @@ sha256="$(nix-prefetch-url \
 	--unpack https://github.com/trevorwhitney/tw-tmux-lib/archive/main.tar.gz)"
 
 cat <<EOF
-{
-  owner = "trevorwhitney";
-  repo = "tw-tmux-lib";
-  rev = "main";
-  sha256 = "${sha256}";
-}
+sha256 = "${sha256}";
 EOF
